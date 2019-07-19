a99c7294c5107144073bf871f5f52cc74f02efce
Alex Hesketh <alex@hesketh.io>
2019-07-19 23:26:00 +0100

    + Kinematic Pawn movement system

---
+++ Assets/[Hesketh]/MechLogic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace BattleBots
+{
+    public class MechLogic : PawnLogic
+    {
+        [Header("Components")]
+        [SerializeField] private PawnMovement pawnMovement = null;
+
+        [Header("Actions")]
+        [SerializeField] private InputActionReference move = null;
+
+        /// <summary>
+        /// A Vector2 value of the input joysticks x,y state
+        /// Should be normalised before this point by the input system
+        /// </summary>
+        public void Input_Move(InputAction.CallbackContext context)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+            pawnMovement.Direction = new Vector3(input.x, 0.0f, input.y);
+        }
+
+        protected override void DeregisterActions()
+        {
+            playerInput.actions.FindAction(move.name).performed -= Input_Move;
+            playerInput.actions.FindAction(move.name).canceled -= Input_Move;
+        }
+
+        protected override void RegisterActions()
+        {
+            playerInput.actions.FindAction(move.name).performed += Input_Move;
+            playerInput.actions.FindAction(move.name).canceled += Input_Move;
+        }
+    }
+}
---
+++ Assets/[Hesketh]/PawnLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem.Plugins.PlayerInput;
+
+namespace BattleBots
+{
+    public abstract class PawnLogic : MonoBehaviour
+    {
+        [Header("Action Map")]
+        [SerializeField] private string actionMapName = "";
+        [SerializeField] private bool activateOnEnable = true;
+
+        protected PlayerInput playerInput = null;
+
+        protected virtual void Awake()
+        {
+            playerInput = GetComponentInParent<PlayerInput>();
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (!string.IsNullOrWhiteSpace(actionMapName) && activateOnEnable)
+            {
+                playerInput.SwitchCurrentActionMap(actionMapName);
+            }
+
+            RegisterActions();
+        }
+
+        protected virtual void OnDisable()
+        {
+            DeregisterActions();
+        }
+
+        protected abstract void RegisterActions();
+        protected abstract void DeregisterActions();
+    }
+}
