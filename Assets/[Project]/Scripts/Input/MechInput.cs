60171dd48d416193df2ff2cc62734a121fcf808a
Alex Calvert <alexcalvert123@live.co.uk>
2019-09-25 22:14:56 +0100

    * Minor Mech input changes

--- Assets/[Project]/Scripts/Input/MechInput.cs
+++ Assets/[Project]/Scripts/Input/MechInput.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using KinematicCharacterController;
 
 namespace BattleBots
 {
-	internal class MechInput : MonoBehaviour, InputActions.IMechActions
+	internal class MechInput : MonoBehaviour, InputActions.IMechActions, ICharacterController
 	{
+		[Header("Speed")]
+		[SerializeField] private float moveSpeed = 10.0f;
+		[SerializeField] private float airMoveMultiplier = 0.75f;
+		[SerializeField] private float turnSpeed = 12.0f;
+
+		[Header("Components")]
+		[SerializeField] private Animator animator = null;
+
 		private InputActions inputActions;
+
+		private KinematicCharacterMotor motor = null;
+
+		private Vector3 accumulatedGravity = Vector3.zero;
 
 		private Vector2 movementInput = Vector2.zero;
 
 		private void Awake()
 		{
+			motor = GetComponent<KinematicCharacterMotor>();
+
 			inputActions = new InputActions();
 			inputActions.Mech.SetCallbacks(this);
+		}
+
+		private void Start()
+		{
+			motor.CharacterController = this;
 		}
 
 		private void OnEnable()
@@ -50,6 +70,53 @@ namespace BattleBots
 		public void OnDash(InputAction.CallbackContext context)
 		{
 			Debug.Log("Dash");
+		}
+
+		public void UpdateRotation(ref Quaternion currentRotation, float deltaTime)
+		{
+			if (movementInput.sqrMagnitude > 0f)
+			{
+				currentRotation = Quaternion.Slerp(currentRotation, Quaternion.LookRotation(new Vector3(movementInput.x, 0f, movementInput.y), Vector3.up), deltaTime * turnSpeed);
+			}
+		}
+
+		public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime)
+		{
+			currentVelocity.x = movementInput.x * moveSpeed;
+			currentVelocity.z = movementInput.y * moveSpeed;
+		}
+
+		public void BeforeCharacterUpdate(float deltaTime)
+		{
+		}
+
+		public void PostGroundingUpdate(float deltaTime)
+		{
+		}
+
+		public void AfterCharacterUpdate(float deltaTime)
+		{
+		}
+
+		public bool IsColliderValidForCollisions(Collider coll)
+		{
+			return true;
+		}
+
+		public void OnGroundHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		{
+		}
+
+		public void OnMovementHit(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, ref HitStabilityReport hitStabilityReport)
+		{
+		}
+
+		public void ProcessHitStabilityReport(Collider hitCollider, Vector3 hitNormal, Vector3 hitPoint, Vector3 atCharacterPosition, Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport)
+		{
+		}
+
+		public void OnDiscreteCollisionDetected(Collider hitCollider)
+		{
 		}
 	}
 }
