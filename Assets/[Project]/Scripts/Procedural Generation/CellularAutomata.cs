fcec4c019b4a55e4aac1a4724a6e83a65ece605d
Alex Calvert <alexcalvert123@live.co.uk>
2019-12-19 17:03:06 +0000

    + New assets

---
+++ Assets/[Project]/Scripts/Procedural Generation/CellularAutomata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+namespace BattleBots
+{
+	internal class CellularAutomata : MonoBehaviour 
+	{
+		[SerializeField] private Tilemap tilemap;
+
+		[SerializeField] private Vector2Int gridSize;
+
+		[SerializeField] private float fillPercentage;
+
+		private bool[,] grid;
+
+		private void Awake()
+		{
+			grid = new bool[gridSize.x, gridSize.y];
+		}
+
+		private void InitialiseRandomGrid()
+		{
+			Random.InitState(DateTime.Now.Millisecond);
+
+			for(int i = 0; i < gridSize.y; i++)
+			{
+				for(int j = 0; j < gridSize.x; j++)
+				{
+
+				}
+			}
+		}
+
+		private void InitialiseSeededGrid(string seed)
+		{
+
+		}
+	}
+}
